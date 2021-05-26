34225e53230839b1b47c673cba607062c92fb776
Christian McGravey <christian@amazing.com>
2021-05-25 22:09:43 -0400

    Update index get.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,12 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/test')
-  getTest(): string {
-    return 'Testing';
+  index() {
+    return this.appService.index();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+
+  index() {
+    return {
+      server: {
+        api: 'NestJS Base',
+        version: '0.1.0',
+        serverTime: new Date(),
+      },
+      config: {
+        http2:
+          this.configService.get<string>('HTTP2') === 'true' &&
+          this.configService.get<string>('HTTPS') === 'true',
+        https: this.configService.get<string>('HTTPS') === 'true',
+      },
+    };
   }
 }
